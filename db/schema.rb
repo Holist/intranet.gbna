6a033f2777d26676161ab0eb76c8bd3570ce44bc
Romain Neuville <neuvilleromain@gmail.com>
2017-10-09 15:50:11 +0200

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009121946) do
+ActiveRecord::Schema.define(version: 20171009134323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "options", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
@@ -24,6 +30,15 @@ ActiveRecord::Schema.define(version: 20171009121946) do
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
+  create_table "user_options", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "option_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["option_id"], name: "index_user_options_on_option_id"
+    t.index ["user_id"], name: "index_user_options_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,6 +55,9 @@ ActiveRecord::Schema.define(version: 20171009121946) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "ldap_imported"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
@@ -53,4 +71,6 @@ ActiveRecord::Schema.define(version: 20171009121946) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "user_options", "options"
+  add_foreign_key "user_options", "users"
 end
