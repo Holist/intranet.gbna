cd129cfdfd250b6e634857770424d0d3eef98958
Romain Neuville <neuvilleromain@gmail.com>
2017-10-11 12:06:12 +0200

    devise rgisterable and recoverable disabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
