8f6976c39b6be2c00ee514542d9c39f4f99a4c21
Romain Neuville <neuvilleromain@gmail.com>
2017-10-11 16:59:39 +0200

    added tools/admin scope in routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '/tools' do
+    scope '/admin' do
+      root to: 'pages#home'
+    end
+  end
+
 end
