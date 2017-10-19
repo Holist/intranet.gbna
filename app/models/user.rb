d112ed8c025245654ec445b0153b322858952b48
Romain Neuville <neuvilleromain@gmail.com>
2017-10-19 16:12:26 +0200

    cards and options ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,21 @@ class User < ApplicationRecord
     end
   end
 
+  def first_name_if_exist
+    # return first_name if exist and different of username
+    return first_name unless username == first_name
+  end
+
+  def last_name_if_exist
+    # return last_name if exist and different of username
+    return last_name unless username == last_name
+  end
+
+  def options_values
+    options_values = {}
+    options.each do |option|
+      options_values[option.name] = UserOption.find_by(user_id: self.id, option_id: option.id).value
+    end
+    options_values
+  end
 end
